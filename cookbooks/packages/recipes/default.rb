579cbfd927062c3954b0e6cf42f4894fe71104da
Ruy Diaz <diazruy@gmail.com>
2012-03-01 20:06:10 -0800

    Adds packages cookbook

---
+++ cookbooks/packages/recipes/default.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: packages
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "build-essential"
+package "gnome-do"
+package "git-core"
+package "git-cola"
+package "ruby-dev"
+package "rubygems"
+package "rake"
+package "curl"
+package "vim-gnome"
+package "exuberant-ctags"
+package "screen"
+package "libopenssl-ruby"
+package "bison"
+package "openssl"
+package "libreadline6"
+package "libreadline6-dev"
+package "zlib1g"
+package "zlib1g-dev"
+package "libssl-dev"
+package "libyaml-dev"
+package "libsqlite3-0"
+package "libsqlite3-dev"
+package "sqlite3"
+package "libxml2-dev"
+package "libxslt-dev"
+package "autoconf"
+package "postgresql"
+package "postgresql-client"
+package "libpq-dev"
+package "libc6-dev"
+package "ncurses-dev"
+package "ncurses-term"
+package "nodejs"
+package "libqt4-dev"
+package "qt4-qmake"
